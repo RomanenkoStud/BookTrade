247cc85ac0b1dc500a16cf0d457d91bcb264cbff
RomanenkoStud <pavlo.romanenko@nure.ua>
2022-09-29 18:59:15 +0300

    First commit 	deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	deleted:    src/index.css     modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	modified:   src/index.js 	public/images/ 	public/style/ 	src/components/ 	src/data.js 	src/style/

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="content">
+			<div class="content-top">
+				<div class="content-top__category">Travel</div>
+				<h1 class="content-top__title">Donec sed odio dui. Cras mattis consectetur purus sit amet fermentum.</h1>
+			</div>
+			<div class="content-bottom">
+				<div class="content-bottom__text">
+					<p>
+						Donec aredsed odio dui. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla.
+					</p>
+					<p>
+						Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. 
+					</p>	
+				</div>
+				<div class="content-bottom-info">
+					<ul class="content-bottom-info-list">
+						<li>Put on this page information about your product</li>
+						<li>A detailed description of your product</li>
+						<li>Tell us about the advantages and merits</li>
+						<li><span>Associate the page with the payment system</span></li>
+					</ul>
+					<a id="button" href="/">
+						<span>Full Story</span>
+					</a>
+				</div>
+			</div>
+		</div>
     </div>
   );
 }
