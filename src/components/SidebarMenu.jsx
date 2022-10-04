4b0a61921892962e010bf1059e4aecd2078a2535
RomanenkoStud <pavlo.romanenko@nure.ua>
2022-10-04 17:41:41 +0300

    Some "href" fix

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -3,19 +3,19 @@ import React from "react";
 function SidebarMenu(props) {
     return (
         <div className="sidenav">
-			<a href="javascript:void(0)" 
+			<a href="/"
 				className={props.active === 'dashboard' ? 'selected' : ''} 
-				onClick={()=>{props.activate("dashboard")}}>My Collection</a>
-			<a href="javascript:void(0)" 
+				onClick={(e)=>{e.preventDefault(); props.activate("dashboard")}}>My Collection</a>
+			<a href="/"
 				className={props.active === 'browse' ? 'selected' : ''} 
-				onClick={()=>{props.activate("browse")}}>Browse</a>
-			<a href="javascript:void(0)" 
+				onClick={(e)=>{e.preventDefault(); props.activate("browse")}}>Browse</a>
+			<a href="/"
 				className={props.active === 'history' ? 'selected' : ''} 
-				onClick={()=>{props.activate("history")}}>History</a>
-			<a href="javascript:void(0)" 
+				onClick={(e)=>{e.preventDefault(); props.activate("history")}}>History</a>
+			<a href="/"
 				className={props.active === 'profile' ? 'selected' : ''} 
-				onClick={()=>{props.activate("profile")}}>Profile</a>
-			<a id="logout" href="javascript:void(0)" onClick={()=>{props.setLogin(false)}}>Logout</a>
+				onClick={(e)=>{e.preventDefault(); props.activate("profile")}}>Profile</a>
+			<a id="logout" href="/" onClick={(e)=>{e.preventDefault(); props.setLogin(false)}}>Logout</a>
 		</div>
     );
 }
